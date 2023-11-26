949a9b813a600435d4edb35c11a210245565c7c3
Josh Moore <jmoore@contelec.com>
2023-11-26 16:51:06 -0600

    gauntlet update: added sound effects. Still have problem where game freezes up occasionally. Occurs only when there are button presses on Joy Featherwing input? https://learn.adafruit.com/joy-featherwing

--- gauntlet/gauntlet.ino
+++ gauntlet/gauntlet.ino
@@ -3,6 +3,8 @@
 #include <Adafruit_DotStarMatrix.h>
 #include <Adafruit_DotStar.h>
 #include <Fonts/TomThumb.h>
+#include <SD.h>
+#include <Adafruit_VS1053.h>
 
 #include "Adafruit_seesaw.h"
 
@@ -65,6 +67,65 @@ uint32_t button_mask = (1 << BUTTON_RIGHT) | (1 << BUTTON_DOWN) |
 
 // scrolling on DotStar LED matrix
 #define SHIFTDELAY 100
+
+// sound module: Adafruit Music Maker FeatherWing
+// (3357 headphone version, or 1788 3W stereo amp version)
+// These are the pins used
+#define VS1053_RESET   -1     // VS1053 reset pin (not used!)
+
+// Feather ESP8266
+#if defined(ESP8266)
+  #define VS1053_CS      16     // VS1053 chip select pin (output)
+  #define VS1053_DCS     15     // VS1053 Data/command select pin (output)
+  #define CARDCS          2     // Card chip select pin
+  #define VS1053_DREQ     0     // VS1053 Data request, ideally an Interrupt pin
+
+// Feather ESP32
+#elif defined(ESP32) && !defined(ARDUINO_ADAFRUIT_FEATHER_ESP32S2)
+  #define VS1053_CS      32     // VS1053 chip select pin (output)
+  #define VS1053_DCS     33     // VS1053 Data/command select pin (output)
+  #define CARDCS         14     // Card chip select pin
+  #define VS1053_DREQ    15     // VS1053 Data request, ideally an Interrupt pin
+
+// Feather Teensy3
+#elif defined(TEENSYDUINO)
+  #define VS1053_CS       3     // VS1053 chip select pin (output)
+  #define VS1053_DCS     10     // VS1053 Data/command select pin (output)
+  #define CARDCS          8     // Card chip select pin
+  #define VS1053_DREQ     4     // VS1053 Data request, ideally an Interrupt pin
+
+// WICED feather
+#elif defined(ARDUINO_STM32_FEATHER)
+  #define VS1053_CS       PC7     // VS1053 chip select pin (output)
+  #define VS1053_DCS      PB4     // VS1053 Data/command select pin (output)
+  #define CARDCS          PC5     // Card chip select pin
+  #define VS1053_DREQ     PA15    // VS1053 Data request, ideally an Interrupt pin
+
+#elif defined(ARDUINO_NRF52832_FEATHER )
+  #define VS1053_CS       30     // VS1053 chip select pin (output)
+  #define VS1053_DCS      11     // VS1053 Data/command select pin (output)
+  #define CARDCS          27     // Card chip select pin
+  #define VS1053_DREQ     31     // VS1053 Data request, ideally an Interrupt pin
+
+// Feather RP2040
+#elif defined(ARDUINO_ADAFRUIT_FEATHER_RP2040)
+  #define VS1053_CS       8     // VS1053 chip select pin (output)
+  #define VS1053_DCS     10     // VS1053 Data/command select pin (output)
+  #define CARDCS          7     // Card chip select pin
+  #define VS1053_DREQ     9     // VS1053 Data request, ideally an Interrupt pin
+
+// Feather M4, M0, 328, ESP32S2, nRF52840 or 32u4
+#else
+  #define VS1053_CS       6     // VS1053 chip select pin (output)
+  #define VS1053_DCS     10     // VS1053 Data/command select pin (output)
+  #define CARDCS          5     // Card chip select pin
+  // DREQ should be an Int pin *if possible* (not possible on 32u4)
+  #define VS1053_DREQ     9     // VS1053 Data request, ideally an Interrupt pin
+
+#endif
+
+Adafruit_VS1053_FilePlayer musicPlayer =
+  Adafruit_VS1053_FilePlayer(VS1053_RESET, VS1053_CS, VS1053_DCS, VS1053_DREQ, CARDCS);
 
 // MATRIX DECLARATION:
 // Parameter 1 = width of DotStar matrix
@@ -337,6 +398,67 @@ void start_game(void) {
   print_screen();
 #endif
   show_screen();
+  musicPlayer.startPlayingFile("/ding.mp3");
+}
+
+/// File listing helper
+// print files on SD card, which should include sound files for this game
+void printDirectory(File dir, int numTabs) {
+   while(true) {
+
+     File entry =  dir.openNextFile();
+     if (! entry) {
+       // no more files
+       //Serial.println("**nomorefiles**");
+       break;
+     }
+     for (uint8_t i=0; i<numTabs; i++) {
+       Serial.print('\t');
+     }
+     Serial.print(entry.name());
+     if (entry.isDirectory()) {
+       Serial.println("/");
+       printDirectory(entry, numTabs+1);
+     } else {
+       // files have sizes, directories do not
+       Serial.print("\t\t");
+       Serial.println(entry.size(), DEC);
+     }
+     entry.close();
+   }
+}
+
+void init_sound() {
+  if (! musicPlayer.begin()) { // initialise the music player
+     Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
+     while (1);
+  }
+
+  Serial.println(F("VS1053 found"));
+
+  musicPlayer.sineTest(0x44, 500);    // Make a tone to indicate VS1053 is working
+
+  if (!SD.begin(CARDCS)) {
+    Serial.println(F("SD failed, or not present"));
+    while (1);  // don't do anything more
+  }
+  Serial.println("SD OK!");
+
+  // list files
+  printDirectory(SD.open("/"), 0);
+
+  // Set volume for left, right channels. lower numbers == louder volume!
+  musicPlayer.setVolume(10,10);
+
+#if defined(__AVR_ATmega32U4__)
+  // Timer interrupts are not suggested, better to use DREQ interrupt!
+  // but we don't have them on the 32u4 feather...
+  musicPlayer.useInterrupt(VS1053_FILEPLAYER_TIMER0_INT); // timer int
+#else
+  // If DREQ is on an interrupt pin we can do background
+  // audio playing
+  musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);  // DREQ int
+#endif
 }
 
 void setup() {
@@ -350,6 +472,7 @@ void setup() {
   Serial.println("Joy FeatherWing basic");
   Serial.println("with Dotstar Matrix Wing");
 #endif
+  init_sound();
 
   if(!ss.begin(0x49)){
 #ifdef DEBUG_CONTROLS
@@ -366,9 +489,9 @@ void setup() {
   ss.pinModeBulk(button_mask, INPUT_PULLUP);
   ss.setGPIOInterrupts(button_mask, 1);
 
-  pinMode(IRQ_PIN, INPUT);
-
-  matrix.begin();
+  //pinMode(IRQ_PIN, INPUT); // Note: Can't do this, or it stops all sound effects from playing afterwards.
+  
+  matrix.begin();  
   matrix.setFont(&TomThumb);
   matrix.setTextWrap(false);
   matrix.setBrightness(bright_dim);
@@ -380,6 +503,7 @@ void setup() {
   }
   matrix.fillScreen(0);
   matrix.show();
+
   start_game();
 }
 
@@ -387,10 +511,12 @@ void
 check_player(void) {
   if (screen_dots[player_x][player_y] == COLOR_PELLET) {
     player_pellets++;
+    musicPlayer.startPlayingFile("/bubble.mp3");
   }
   if (screen_dots[player_x][player_y] == COLOR_WALL) {
     screen_dots[player_x][player_y] = COLOR_EXPLOSION;
     player_dead = true;
+    musicPlayer.startPlayingFile("/birds.mp3");
   } else {
     // show player again
     screen_dots[player_x][player_y] = COLOR_PLAYER;
@@ -553,6 +679,7 @@ void loop() {
     if (move_x != 0 || move_y != 0) { // player is moving
       // erase where thc player was
       screen_dots[player_x][player_y] = COLOR_BLACK;
+      //musicPlayer.startPlayingFile("/track001.mp3");
     }
     player_x += move_x;
     if (player_x < 0) {
